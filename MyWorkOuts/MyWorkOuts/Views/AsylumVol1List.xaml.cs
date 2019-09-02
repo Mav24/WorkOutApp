2c38ba057e02ed8e98c4508788c4980e1298a3c1
Maverick <murrayduke@rogers.com>
2019-09-02 14:14:06 -0400

    Some UI updates and App Icon was changed. Started adding in a Program list option. To list all the avalible programs.

---
+++ MyWorkOuts/MyWorkOuts/Views/AsylumVol1List.xaml.cs
@@ -0,0 +1,25 @@
+﻿using MyWorkOuts.Data;
+using MyWorkOuts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyWorkOuts.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AsylumVol1List : ContentPage
+    {
+        public AsylumVol1List()
+        {
+            InitializeComponent();
+            List<string> workOutList = BeachBodyWorkOutList.AsylumVol1();
+
+            MyAsylumVol1WorkOutList.ItemsSource = BeachBodyWorkOutList.AsylumVol1();
+        }
+    }
+}
