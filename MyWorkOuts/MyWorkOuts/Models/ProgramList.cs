d840263485b44dcd62d9b7969c997af1cca32eae
Maverick <murrayduke@rogers.com>
2019-09-08 20:07:37 -0400

    UI updates to the Program page

---
+++ MyWorkOuts/MyWorkOuts/Models/ProgramList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyWorkOuts.Models
+{
+    public class ProgramList
+    {
+        public string ProgramTitle { get; set; }
+
+        public string DaysToWorkOut { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
--- MyWorkOuts/MyWorkOuts/Views/WorkOutPrograms.xaml.cs
+++ MyWorkOuts/MyWorkOuts/Views/WorkOutPrograms.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyWorkOuts.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,20 @@ namespace MyWorkOuts.Views
         public WorkOutPrograms()
         {
             InitializeComponent();
+            MyProgramsList.ItemsSource = new List<ProgramList>
+            {
+                new ProgramList { ProgramTitle = "Insanity", DaysToWorkOut = "63 Day's", ImageUrl = "insanity1.png" },
+                new ProgramList { ProgramTitle = "Asylum Vol 1", DaysToWorkOut = "30 Day's", ImageUrl = "asylumvol1.png" },
+                new ProgramList { ProgramTitle = "Asylum Vol 2", DaysToWorkOut = "28 Day's", ImageUrl = "asylumvol2.png" },
+                new ProgramList { ProgramTitle = "Asylum 1 + 2", DaysToWorkOut = "55 Day's", ImageUrl = "asylumoneandtwo.png" },
+                new ProgramList { ProgramTitle = "Ten Minute Trainer", DaysToWorkOut = "28 Day's", ImageUrl = "tenminutetrainer1.png" },
+                new ProgramList { ProgramTitle = "Max 30", DaysToWorkOut = "56 Day's", ImageUrl = "max30.png" }
+            };
+        }
+
+        private void WorkoutProgramsList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
         }
     }
 }
